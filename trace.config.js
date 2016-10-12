e4b7937069258e769ae5edcb3f6b29533756ac17
sdromacque <sdromacquedev@gmail.com>
2016-10-12 16:51:27 +0200

    Trace config

---
+++ trace.config.js
@@ -0,0 +1,5 @@
+// trace.config.js
+module.exports = {
+    serviceName: 'test',
+    apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3ZmNkZjk4Y2U4NTBmMDAxMDVhYTc2NSIsImlhdCI6MTQ3NjE5MDEwNH0.yiuAMy9jtUmDGMJOVhvml-OJ-bCIcw39Aev4V7QTwgU'
+};
